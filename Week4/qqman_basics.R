c22fa7f1ed414e4073274c88b3572224c9252ad4
flemm0 <flemming.wu@gmail.com>
2022-09-13 19:14:37 -0700

    finishing up qqman walkthrough in activity 2B

--- Week4/qqman_basics.R
+++ Week4/qqman_basics.R
@@ -4,7 +4,7 @@ install.packages("devtools")
 
 require(devtools)
 
-install_version("qqman", version = "0.1.2", repos = "http://cran.us.rproject.org")
+install_version("qqman", version = "0.1.2", repos = "http://cran.us.r-project.org")
 
 library("qqman")
 
@@ -23,3 +23,34 @@ vignette("qqman")
 as.data.frame(table(gwasResults$CHR))
 
 manhattan(gwasResults)
+
+manhattan(gwasResults, main = "Manhattan Plot", ylim = c(0, 10), cex = 0.6, cex.axis = 0.9, col = c("blue4", "orange3"), suggestiveline = F, genomewideline = F, chrlabs = c(1:20, "P", "Q"))
+
+
+manhattan(subset(gwasResults, CHR == 1))
+#My plot resembles my instructor's plot.
+
+##The vignette's graph has points scattered around the plot, but my plot has points on a straight line through the x-axis. Additionally, the ticks on the x-axis seem to be in Mb ranging from -1.0 to 1.0, whereas the x-axis in the vignette plot seem to be in bases, ranging from 0 to 1500.
+
+
+str(snpsOfInterest) #can be an user-generated input of any list of SNPs
+
+manhattan(subset(gwasResults, CHR == 3), highlight = snpsOfInterest, xlim = c(200, 500), main = "Chr 3")
+#Again, my plot resembles that of my instructor's instead of that of the vignette. I notice the same issue with the scale of the x-axis (which is in Mb in my plot) being different than in the vignette, which is likely in base pairs.
+
+install.packages("qqman")
+
+library(qqman)
+
+manhattan(subset(gwasResults, CHR == 1))
+#Plot looks correct now.
+
+
+manhattan(subset(gwasResults, CHR == 3), highlight = snpsOfInterest, xlim = c(200, 500), main = "Chr 3")
+#Plot looks correct.
+
+manhattan(gwasResults, highlight = snpsOfInterest)
+
+manhattan(gwasResults, annotatePval = 0.01) #labels top SNP per chromosome that exceeds p-value threshold (default action)
+
+manhattan(gwasResults, annotatePval = 0.005, annotateTop = FALSE) #excludes top SNP on each chromosome

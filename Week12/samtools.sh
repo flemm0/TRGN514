b44b383cd9914fb31458c5c7b516eb5eeed72e73
flemm0 <flemming.wu@gmail.com>
2022-11-10 12:54:33 -0800

    merged read counts into single file

--- Week12/samtools.sh
+++ Week12/samtools.sh
@@ -1,10 +1,12 @@
 # samtools index all bam files
 
+cd /scratch/wuflemmi-F22/
+
 mkdir Activity_4
 
 cd Activity_4
 
-nohup cp -R /scratch/Illumina-Human-Body-Map-BAMS . &
+nohup cp -R /scratch/Illumina-Human-Body-Map-BAMS . & #all .bam files are in this directory, copy to current directory
 
 cd Illumina-Human-Body-Map-BAMS
 
@@ -12,3 +14,45 @@ for file in *.bam
 do 
 	nohup samtools index $file &
 done
+
+mv * ..
+
+rmdir Illumina-Human-Body-Map-BAMS
+
+
+# create symbolic links for 5 selected bam and index files:
+## adipose, breast, colon, brain, liver
+
+ln -s /scratch/Illumina-Human-Body-Map-BAMS/ERR030880_F73_Adipose.proj.accepted_hits.bam Adipose
+ln -s /scratch/Illumina-Human-Body-Map-BAMS/ERR030883_F29_Breast.proj.accepted_hits.bam Breast
+ln -s /scratch/Illumina-Human-Body-Map-BAMS/ERR030884_F68_Colon.proj.accepted_hits.bam Colon
+ln -s /scratch/Illumina-Human-Body-Map-BAMS/ERR030882_F77_Brain.proj.accepted_hits.bam Brain
+ln -s /scratch/Illumina-Human-Body-Map-BAMS/ERR030887_M37_Liver.proj.accepted_hits.bam Liver
+
+
+ln -s ERR030880_F73_Adipose.proj.accepted_hits.bam.bai Adipose.bai
+ln -s ERR030883_F29_Breast.proj.accepted_hits.bam.bai Breast.bai
+ln -s ERR030884_F68_Colon.proj.accepted_hits.bam.bai Colon.bai
+ln -s ERR030882_F77_Brain.proj.accepted_hits.bam.bai Brain.bai
+ln -s ERR030887_M37_Liver.proj.accepted_hits.bam.bai Liver.bai
+
+
+# run samtools idxstats on the 5 sets of files
+
+samtools idxstats Adipose > Adipose.idxstats
+samtools idxstats Breast > Breast.idxstats
+samtools idxstats Colon > Colon.idxstats
+samtools idxstats Brain > Brain.idxstats
+samtools idxstats Liver > Liver.idxstats
+
+
+### idxstats columns are 1: ref sequence name, 2: chromosome size, 3: # of mapped reads, 4: # of unmapped reads
+
+# create a merged text file that has the first column from any of the idxstats file (the chromosome name) and the # of mapped reads from each of the 5 samples as their own column
+
+paste Adipose.idxstats Breast.idxstats Colon.idxstats Brain.idxstats Liver.idxstats | awk 'BEGIN {print "Chromosome", "Adipose", "Breast", "Colon", "Brain", "Liver" } { print $1, $3, $7, $11, $15, $19 }' > 5-Tissues.mapped.idxstats.txt
+
+# line up columns by running the following command in command mode on vim on the new file:
+## :%!column -t
+## save: :wq
+
